5b47054024c09faae6cb69b3c6edcffb21e53111
EC2 Default User <ec2-user@ip-172-31-93-156.ec2.internal>
2026-06-08 19:30:44 +0000

    init3

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,8 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 sudo yum install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
-
+pip3 install --upgrade pip
+pip3 install sagemaker==3.13.1 --no-deps sagemaker-core sagemaker-train sagemaker-serve sagemaker-mlops
 pip3 install -r requirements.txt  -v
 
 
